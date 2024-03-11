6f69164f2f56043f673e48837377fb8ee42e87db
quasar098 <70716985+quasar098@users.noreply.github.com>
2024-03-10 20:13:34 -0400

    cangerjun comes in clutch

--- translations.py
+++ translations.py
@@ -25,7 +25,7 @@ TRANSLATIONS = {
 
         "back": "Back",
         "title": "midi-playground",
-        "title-marquee": """Contributors: quasar098, TheCodingCrafter, PurpleJuiceBox, sled45, Times0. Just want to 
+        "title-marquee": """Contributors: quasar098, TheCodingCrafter, PurpleJuiceBox, sled45, Times0, Spring-Forever-with-me. Just want to 
 watch the square and don't want to play the game? Turn on theatre mode in the config if that is the case. Interested in playing your 
 own song but don't know how? Check out docs/SONGS.md in the source code to learn how to add your own songs.""".replace("\n", ''),
 
@@ -44,7 +44,7 @@ own song but don't know how? Check out docs/SONGS.md in the source code to learn
 
         "back": "返回",
         "title": "midi游乐场",
-        "title-marquee": """贡献者: quasar098, TheCodingCrafter, PurpleJuiceBox, sled45, Times0. 快来加入我们！ 
+        "title-marquee": """贡献者: quasar098, TheCodingCrafter, PurpleJuiceBox, sled45, Times0, Spring-Forever-with-me, cangerjun. 快来加入我们！ 
 想要看着方块自己动，不想玩游戏？
 如果是这样，请在设置中打开剧场模式。
 想要播放自己喜欢的歌曲，但不知道如何播放？
