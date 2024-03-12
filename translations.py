916096983a14a9e58b9079807734783cc660fb03
Lucas Gabriel <90426410+lucmsilva651@users.noreply.github.com>
2024-03-12 18:28:09 -0300

    Brazilian Portuguese translation

--- translations.py
+++ translations.py
@@ -12,11 +12,30 @@ TRANSLATIONS = {
 
         "back": "Back",
         "title": "midi-playground",
-        "title-marquee": """Contributors: quasar098, TheCodingCrafter, PurpleJuiceBox, sled45, Times0, Spring-Forever-with-me. Just want to 
+        "title-marquee": """Contributors: quasar098, TheCodingCrafter, PurpleJuiceBox, sled45, Times0, Spring-Forever-with-me cangerjun, lucmsilva651. Just want to 
 watch the square and don't want to play the game? Turn on theatre mode in the config if that is the case. Interested in playing your 
 own song but don't know how? Check out docs/SONGS.md in the source code to learn how to add your own songs.""".replace("\n", ''),
 
         "restart-required": "Restart Required"
+    },
+    "brazilian-pt": {
+        "font": "poppins-regular.ttf",
+
+        "font-menu-shadow-offset": 9,
+        "font-menu-shadow-length-offset": 0,
+        "play": "Jogar",
+        "config": "Configurações",
+        "contribute": "Contribuir",
+        "open-songs-folder": "Abrir Pasta de Músicas",
+        "quit": "Sair",
+
+        "back": "Voltar",
+        "title": "midi-playground",
+        "title-marquee": """Contribuintes: quasar098, TheCodingCrafter, PurpleJuiceBox, sled45, Times0, Spring-Forever-with-me, cangerjun, lucmsilva651. 
+        Quer apenas assistir ao quadrado e não quer jogar o jogo? Se for esse o caso, ative o Modo Teatro nas configurações. Tem interesse em colocar sua própria música, 
+        mas não sabe como? Consulte docs/SONGS.md no código-fonte para saber como adicionar suas próprias músicas.""".replace("\n", ''),
+
+        "restart-required": "Reinicialização Necessária"
     },
     "chinese": {
         "font": "hanyisongjian.ttf",
@@ -31,7 +50,7 @@ own song but don't know how? Check out docs/SONGS.md in the source code to learn
 
         "back": "返回",
         "title": "midi游乐场",
-        "title-marquee": """贡献者: quasar098, TheCodingCrafter, PurpleJuiceBox, sled45, Times0, Spring-Forever-with-me, cangerjun. 快来加入我们！ 
+        "title-marquee": """贡献者: quasar098, TheCodingCrafter, PurpleJuiceBox, sled45, Times0, Spring-Forever-with-me, cangerjun, lucmsilva651. 快来加入我们！ 
 想要看着方块自己动，不想玩游戏？
 如果是这样，请在设置中打开剧场模式。
 想要播放自己喜欢的歌曲，但不知道如何播放？
